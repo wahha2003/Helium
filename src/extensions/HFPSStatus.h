0c2c01ae1470955460ae63557c7af21273b2cd27
wahha2003 <104408266+wahha2003@users.noreply.github.com>
2026-02-08 15:37:51 +0800

    Update HFPSStatus.h

--- src/extensions/HFPSStatus.h
+++ src/extensions/HFPSStatus.h
@@ -1,9 +1,31 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+#import <mach/mach.h>
+
+// 声明 QuartzCore 私有 API
+extern "C" {
+    int CARenderServerGetDebugOption(mach_port_t port, int key);
+    int CARenderServerGetDebugValue(mach_port_t port, int key);
+    void CARenderServerSetDebugOption(mach_port_t port, int key, int value);
+    void CARenderServerSetDebugValue(mach_port_t port, int key, int value);
+}
+
+// 核心常量
+#define CA_DEBUG_OPTION_PERF_HUD 0x24
+
+typedef NS_ENUM(NSInteger, CAPerfHUDLevel) {
+    CAPerfHUDLevelOff = 0,
+    CAPerfHUDLevelBasic = 1,
+    CAPerfHUDLevelBackdrop = 2,
+    CAPerfHUDLevelParticles = 3,
+    CAPerfHUDLevelFull = 4,
+    CAPerfHUDLevelFrequencies = 5,
+    CAPerfHUDLevelPower = 6,
+    CAPerfHUDLevelFPSOnly = 7,
+    CAPerfHUDLevelDisplay = 8,
+    CAPerfHUDLevelGlitches = 9
+};
+
 @interface HFPSStatus : NSObject
-
-@property (nonatomic)float fpsValue;
-
-+ (HFPSStatus *)sharedInstance;
-
++ (void)setPerfHUDLevel:(CAPerfHUDLevel)level;
++ (CAPerfHUDLevel)currentPerfHUDLevel;
 @end
